bfe31d4fbb2abf152de8a3bf06506c7d26f1d2d7
Marc Kirsch <marc.kirsch17@gmail.com>
2019-02-10 14:20:38 -0600

    deployed

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Footer(props) {
+  return (
+    <footer className="page-footer">
+      
+      <div className="footer-copyright">
+        <div className="container center">
+        © 2019 Marc Kirsch
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+
+
+
+export default Footer;
